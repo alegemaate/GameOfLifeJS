3ff39ec1e026ed42d98238f91a86c573f2349e72
alegemaate <alegemaate@gmail.com>
2019-05-13 22:34:00 -0400

    Implemented scaling and ticker

--- js/GameState.js
+++ js/GameState.js
@@ -19,7 +19,9 @@ export default class GameState extends Phaser.Scene {
   create () {
     // Game state variables
     this.paused = true;
-    this.cellSize = 20;
+    this.cellSize = 5;
+    this.tickerTime = 50;
+
     this.pausedText = this.add.text(10, 10, 'PAUSED - Press the SPACE BAR', { font: '16px Courier', fill: '#ffffff' }).setShadow(1, 1);
 
     this.graphics = this.add.graphics({ lineStyle: { width: 4, color: 0xaa00aa } });
@@ -37,26 +39,17 @@ export default class GameState extends Phaser.Scene {
         }
     }, this);
 
-    this.input.on('pointerdown', function (pointer) {
-        this.click(pointer);
+    this.input.on('pointermove', function (pointer) {
+        if(pointer.isDown){
+          this.click(pointer);
+        }
     }, this);
+
+    this.timedEvent = this.time.delayedCall(this.tickerTime, this.ticker, [], this);
   }
 
-  // Update
-  update() {
-    // Draw
-    this.graphics.clear();
-
-    for (var i = 0; i < this.LIFE_W; i++) {
-      for (var t = 0; t < this.LIFE_H; t++) {
-        if (this.l_map[i][t] == 1) {
-          this.graphics.fillStyle(0xFFFFFF, 1.0);
-          this.graphics.fillRect(i * this.cellSize, t * this.cellSize,
-                                 this.cellSize, this.cellSize);
-        }
-      }
-    }
-
+  // Ticker
+  ticker() {
     // Simulate
     if (!this.paused) {
       this.temp_map = Array(this.LIFE_W).fill().map(() => Array(this.LIFE_H).fill(0));
@@ -76,7 +69,6 @@ export default class GameState extends Phaser.Scene {
           // Dies from overpop or underpop
           if (neighbours < 2 || neighbours > 3)
              this.temp_map[i][t] = 0;
-
           // Is born
           else if (neighbours == 3)
             this.temp_map[i][t] = 1;
@@ -88,6 +80,25 @@ export default class GameState extends Phaser.Scene {
 
       this.l_map = [...this.temp_map];
     }
+
+    this.timedEvent = this.time.delayedCall(this.tickerTime, this.ticker, [], this);
+
+  }
+
+  // Update
+  update() {
+    // Draw
+    this.graphics.clear();
+
+    for (var i = 0; i < this.LIFE_W; i++) {
+      for (var t = 0; t < this.LIFE_H; t++) {
+        if (this.l_map[i][t] == 1) {
+          this.graphics.fillStyle(0xFFFFFF, 1.0);
+          this.graphics.fillRect(i * this.cellSize, t * this.cellSize,
+                                 this.cellSize, this.cellSize);
+        }
+      }
+    }
   }
 
   // Custom mod
@@ -98,9 +109,9 @@ export default class GameState extends Phaser.Scene {
   // Click event
   click (pointer) {
     if (pointer.buttons == 1)
-      this.l_map[Math.floor(pointer.downX / this.cellSize)][Math.floor(pointer.downY / this.cellSize)] = 1;
+      this.l_map[Math.floor(pointer.x / this.cellSize)][Math.floor(pointer.y / this.cellSize)] = 1;
     else if (pointer.buttons == 2)
-      this.l_map[Math.floor(pointer.downX / this.cellSize)][Math.floor(pointer.downY / this.cellSize)] = 0;
+      this.l_map[Math.floor(pointer.x / this.cellSize)][Math.floor(pointer.y / this.cellSize)] = 0;
   }
 
   // Toggle pause play
@@ -108,8 +119,8 @@ export default class GameState extends Phaser.Scene {
     this.paused = !this.paused;
 
     if (this.paused) 
-      this.pausedText.text = 'PAUSED - Press the SPACE BAR';
+      this.pausedText.text = 'PAUSED - Press SPACE BAR to play';
     else
-      this.pausedText.text = 'PLAYING - Press the SPACE BAR';
+      this.pausedText.text = 'PLAYING - Press SPACE BAR to pause';
   }
 }
--- js/Main.js
+++ js/Main.js
@@ -1,15 +1,35 @@
 import MenuState from "./MenuState.js";
 import GameState from "./GameState.js";
 
-// Config for game
-var config = {
-  type: Phaser.AUTO,
-  width: 600,
-  height: 600,
-  backgroundColor: '#000',
-  parent: "game-container",
-  scene: [ GameState, MenuState ]
-};
+// Window resizing
+function resize(game) {
+	var canvas = document.querySelector("canvas");
+	var windowWidth = window.innerWidth;
+	var windowHeight = window.innerHeight;
+	var windowRatio = windowWidth / windowHeight;
+	var gameRatio = game.config.width / game.config.height;
+
+	if(windowRatio < gameRatio) {
+	  canvas.style.width = windowWidth + "px";
+	  canvas.style.height = (windowWidth / gameRatio) + "px";
+	}
+	else {
+	  canvas.style.width = (windowHeight * gameRatio) + "px";
+	  canvas.style.height = windowHeight + "px";
+	}
+}
 
 // Create game
-var game = new Phaser.Game(config);
+window.onload = function() {
+  var game = new Phaser.Game({
+	  type: Phaser.AUTO,
+	  width: 500,
+	  height: 500,
+	  backgroundColor: '#000',
+	  parent: "game-container",
+	  scene: [ GameState, MenuState ]
+	});
+
+  resize(game);
+  window.addEventListener("resize", () => resize(game), false);
+}
